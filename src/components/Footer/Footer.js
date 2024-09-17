8970f39197cba43c228023c0738f3d3ee741c980
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-09-17 16:39:46 +0300

    gata si cu acest proiect

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,36 @@ import {
 } from './FooterStyles';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel: 111-111-1111">111-111-1111</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:alimuci@gmail.com">alimuci@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>{' '}
+        <SocialContainer>
+          <SocialIcons href="https://github.com">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://instagram.com">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
